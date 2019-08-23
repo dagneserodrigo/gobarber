a26609c1332b29b652bb8f374b6d601869da7813
Rodrigo Dagnese <dagnese.rodrigo@gmail.com>
2019-08-22 23:04:03 -0300

    feat(database): connection and user migration

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'oi' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rodrigo Dagnese',
+    email: 'dagnese.rodrigo@gmail.com',
+    password_hash: 'aaa123',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
